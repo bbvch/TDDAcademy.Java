727060a8c4b77983e9552f6846da01f166c84dbd
Raphael Schweizer <raphael.schweizer@bbv.ch>
2024-04-26 23:59:20 +0200

    update libs

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -18,13 +18,13 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation("com.google.guava:guava:33.1.0-jre")
 }
 
 tasks.named<Test>("test") {
